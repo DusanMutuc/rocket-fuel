934951f541deb0a2cc0b26061dc5b5fade3b8c9d
Dusan Mutuc <mutucdusan@gmail.com>
2025-06-21 00:43:22 +0200

    android persistent login

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-// Replace with your actual Supabase Project URL & Anon Key
 const SUPABASE_URL = 'https://cqlkhkrksvnjorriroyg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNxbGtoa3Jrc3Zuam9ycmlyb3lnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzkzMTY1OTIsImV4cCI6MjA1NDg5MjU5Mn0.BSNkA1A6UT3JRfZGtuN7-4pZS2eLGDYdCp6xg3vm0aw';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
+});
